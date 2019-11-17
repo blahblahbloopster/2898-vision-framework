d1cbef5cb035a5aaad9a396be08c362c0acc44e2
Max Leibowitz <403089@bsd48.org>
2019-11-17 12:36:32 -0800

    Working on making display work when not in pipeline mode

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ from EasyContour import EasyContour
 # to have display output.
 PIPELINE = False
 # This can be a port number if it's an actual camera, or a video file
-CAMERA_ID = ""
-RGB_BOUNDS = (0, 100, 0), (100, 255, 100)
+CAMERA_ID = "camera1_feed.mkv"
+# This changes if it will display some debug windows
+DISPLAY = True
+RGB_BOUNDS = (np.array([0, 100, 0]), np.array([100, 255, 100]))
 MATRIX = None
 DISTORTION = None
 TARGET_DIMENSIONS = EasyContour(((1, 2), (3, 4), (5, 6), (7, 8)))
-TARGET_DIMENSIONS = TARGET_DIMENSIONS.format((("x", "y", 0), ("x", "y", 0)), np.float32)
+TARGET_DIMENSIONS = TARGET_DIMENSIONS.format([["x", "y", 0], ["x", "y", 0]], np.float32)
 if PIPELINE:
     import multiprocessing
 else:
     import multiprocessing.dummy as multiprocessing
 cap = None
+img_org = None
 
 
 def compute_output_values(rotation_vec, translation_vec):
@@ -50,6 +53,7 @@ def compute_output_values(rotation_vec, translation_vec):
 
 
 def get_video(inp):
+    global img_org
     if inp == "stop":
         return "stop"
     elif inp is None:
@@ -57,8 +61,14 @@ def get_video(inp):
     ret, frame = cap.read()
     if not ret:
         return "stop"
+    if not PIPELINE:
+        if DISPLAY:
+            cv2.imshow("Original image", frame)
     # Note: it may be better to do image processing here so that you won't have to wait
     # for memory transfer between processes
+    if not PIPELINE:
+        if DISPLAY:
+            img_org = frame.copy()
     return frame
 
 
@@ -67,13 +77,21 @@ def process_frame(inp):
         return "stop"
     elif inp is None:
         return None
-    frame = cv2.inRange(inp, RGB_BOUNDS[1], RGB_BOUNDS[1])
+    frame = cv2.inRange(inp, RGB_BOUNDS[0], RGB_BOUNDS[1])
+    if not PIPELINE:
+        if DISPLAY:
+            cv2.imshow("In range", frame)
     # Change RETER_EXTERNAL to RETER_TREE if you are getting spotty detection
     contours = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[1]
+    if not PIPELINE:
+        if DISPLAY:
+            cv2.drawContours(img_org, contours, -1, (255, 0, 0), 3)
+            cv2.imshow("Contours", img_org)
     easy_contours = []
     for cnt in contours:
-        easy_contours.append(EasyContour(cnt))
+        if len(cnt) > 5:
+            easy_contours.append(EasyContour(cnt))
     return contours
 
 
@@ -84,17 +102,21 @@ def filtering_and_solving(inp):
         return None
     # This is where you put your code.  Inp is a list of EasyContour objects.
     corners = EasyContour(((1, 2), (3, 4), (5, 6), (7, 8)))
-    solvepnp_formatted = corners.format((("x", "y"), ("x", "y")), np.float32)
-    got_output, rotation_vector, translation_vector = cv2.solvePnP(TARGET_DIMENSIONS, solvepnp_formatted,
-                                                                   MATRIX, DISTORTION)
-    if not got_output:
-        print("Solvepnp failed")
-        return "stop"
-    distance, angle1, angle2 = compute_output_values(rotation_vector, translation_vector)
-    return distance, angle1, angle2
+    solvepnp_formatted = corners.format([["x", "y"], ["x", "y"]], np.float32)
+    # got_output, rotation_vector, translation_vector = cv2.solvePnP(TARGET_DIMENSIONS, solvepnp_formatted,
+    #                                                                MATRIX, DISTORTION)
+    # if not got_output:
+    #     print("Solvepnp failed")
+    #     return "stop"
+    # distance, angle1, angle2 = compute_output_values(rotation_vector, translation_vector)
+    # return distance, angle1, angle2
+    return None
 
 
-def work_function(input_queue, output_queue, function):
+def work_function(input_queue, output_queue, function, camera=False):
+    if camera:
+        global cap
+        cap = cv2.VideoCapture(CAMERA_ID)
     while True:
         returned = function(input_queue.get())
         if returned == "stop":
@@ -104,8 +126,8 @@ def work_function(input_queue, output_queue, function):
 
 
 if __name__ == '__main__':
-    queues = [multiprocessing.Queue()] * 4
-    processes = [multiprocessing.Process(target=work_function, args=(queues[0], queues[1], get_video)),
+    queues = [multiprocessing.Queue(), multiprocessing.Queue(), multiprocessing.Queue(), multiprocessing.Queue()]
+    processes = [multiprocessing.Process(target=work_function, args=(queues[0], queues[1], get_video, True)),
                  multiprocessing.Process(target=work_function, args=(queues[1], queues[2], process_frame)),
                  multiprocessing.Process(target=work_function, args=(queues[2], queues[3], filtering_and_solving))]
     for i in range(3):
